4d7ce7dbc844de902e9dbd418ca971ec10cc1620
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-19 14:33:04 +0900

    Create InputTest.java

---
+++ day03/src/day03/InputTest.java
@@ -0,0 +1,35 @@
+package day03;
+
+//슨배님들이 만든 Scanner 클래스 쓰겠다고 현재 파일에 추가!
+import java.util.Scanner;
+
+public class InputTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("이름 : ");
+		String name = sc.next();
+		System.out.println(name+"님 어서오세요~");
+		
+		System.out.print("주소 : ");
+		//위에서 내려오는 enter를 먹어치우는 역할
+		sc.nextLine();
+		String addr = sc.nextLine();
+		System.out.println("당신이 사는곳은 "+addr+" 입니다.");
+		
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		System.out.println("당신의 5년뒤 나이는 "+(age+5)+"살입니다.");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
