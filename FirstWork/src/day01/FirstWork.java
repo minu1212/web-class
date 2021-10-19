d2d67dbe637dbde1d1b2b86473dded75eea916a8
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-19 14:33:11 +0900

    Create FirstWork.java

---
+++ FirstWork/src/day01/FirstWork.java
@@ -0,0 +1,19 @@
+package day01;
+//주석
+//해석하고 싶지 않은 문장 앞에 사용한다.
+//1. 소스코드에 설명하는 글을 달 때(개발자들간의 소통)
+//2. 기존에 사용하던 소스코드를 더 이상 사용하지 않을 때
+//한줄 주석
+/*
+여러줄
+주석
+*/
+//Ctrl + +(-) : 글자 확대(축소)
+//Ctrl + F11 : 실행하기
+public class FirstWork {
+	//main 메소드 : 프로그램의 시작점, 진입점 
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+
+}
