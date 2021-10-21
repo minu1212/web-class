20c97c6489b5598725eafec15187df020692e54c
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-21 17:03:28 +0900

    Update ArrayTest.java

--- day05/src/day05/ArrayTest.java
+++ day05/src/day05/ArrayTest.java
@@ -7,21 +7,21 @@ public class ArrayTest {
 		for (int i = 0; i < 5; i++) {
 			System.out.println(arData[i]);
 		}
-		
+//		
 //		int[] arData2 = new int[5];
-////		arData2[0] = 1;
-////		arData2[1] = 2;
-////		arData2[2] = 3;
-////		arData2[3] = 4;
-////		arData2[4] = 5;
-//		for (int i = 0; i < 5; i++) {
-//			arData2[i] = i+1;
-//		}
+//		arData2[0] = 1;
+//		arData2[1] = 2;
+//		arData2[2] = 3;
+//		arData2[3] = 4;
+//		arData2[4] = 5;
+		for (int i = 0; i < 5; i++) {
+			arData2[i] = i+1;
+		}
 		
-//		int data1 = 10;
-//		int data2 = data1;
-//		data2 += 5;
-//		System.out.println(data1);
+		int data1 = 10;
+		int data2 = data1;
+		data2 += 5;
+		System.out.println(data1);
 		
 		int[] arData2 = {1,2,3,4,5};
 		int[] arData3 = arData2;
