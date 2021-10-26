fd586c32508f3ecc45523d5d7e52e95543d43bbe
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-26 19:11:29 +0900

    Create AccessTest.java

---
+++ day10/src/access1/AccessTest.java
@@ -0,0 +1,24 @@
+package access1;
+
+public class AccessTest {
+	int data1=10;
+	public int data2=20;
+	private int data3=30;
+	protected int data4=40;
+	
+	//Alt + Shift + S > R : getter, setter ¸¸µé±â
+	//getter
+	public int getData3() {
+		return data3;
+	}
+	//setter
+	public void setData3(int data3) {
+		this.data3 = data3;
+	}
+}
+
+
+
+
+
+
