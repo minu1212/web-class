7922ed23cad6c02dbe500e95d0f895630c2713bf
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-26 19:12:20 +0900

    Create Ground.java

---
+++ day10/src/zoo/Ground.java
@@ -0,0 +1,10 @@
+package zoo;
+
+public class Ground {
+	public static void main(String[] args) {
+		Dog dog = new Dog("»Ç»ß", 5);
+		Cat cat = new Cat("Å°Æ¼", 3);
+		dog.makeSomeNoise();
+		cat.makeSomeNoise();
+	}
+}
