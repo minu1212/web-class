79bc82117bc6b1a88e40e73a5070dbc6a6894e80
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-26 19:11:52 +0900

    Create Cookie.java

---
+++ day10/src/mart/Cookie.java
@@ -0,0 +1,11 @@
+package mart;
+
+public interface Cookie {
+//	public final static boolean 질소 = true;
+//	abstract void pack();
+
+//	인터페이스는 상수와 추상메소드만 선언이 가능하기 때문에
+//	그냥 선언해도 상수와 추상메소드로 선언이 된다.
+	boolean 질소 = true;
+	void pack();
+}
