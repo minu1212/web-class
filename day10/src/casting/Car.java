1abd62278b380fa455139e35dfc172bf6e594605
minu1212 <88140406+minu1212@users.noreply.github.com>
2021-10-26 19:11:35 +0900

    Create Car.java

---
+++ day10/src/casting/Car.java
@@ -0,0 +1,18 @@
+package casting;
+
+public class Car {
+	String brand;
+	String color;
+	int price;
+	public Car(String brand, String color, int price) {
+		this.brand = brand;
+		this.color = color;
+		this.price = price;
+	}
+	void engineStart() {
+		System.out.println(brand+" ¿­¼è·Î ½Ãµ¿ÄÑ±â");
+	}
+	void engineStop() {
+		System.out.println(brand+" ¿­¼è·Î ½Ãµ¿²ô±â");
+	}
+}
